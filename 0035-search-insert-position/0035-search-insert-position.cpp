eac94fc4373935eb91afbae3022dd0c356933ddb
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-21 17:50:17 +0500

    Time: 0 ms (100%), Space: 13.7 MB (43.38%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        auto it=lower_bound(nums.begin(),nums.end(),target);
-        return it-nums.begin();
-        
+        // auto it=lower_bound(nums.begin(),nums.end(),target);
+        // return it-nums.begin();
+        // OR
+        int st=0,end=nums.size()-1;
+        int ans=nums.size();
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                end=mid-1;
+            }
+            else{st=mid+1;}
+        }
+        return ans;
     }
 };
