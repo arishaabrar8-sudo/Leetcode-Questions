5f5d125a5227edb0a252236d52ac5398f89c4f43
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-23 18:15:46 +0500

    Time: 0 ms (100%), Space: 17.6 MB (20.2%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.empty()) return {-1, -1};
+    //     if(nums.empty()) return {-1, -1};
 
-       int indexi=-1,indexj=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){indexi=i;
-            break;}
+    //    int indexi=-1,indexj=-1;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[i]==target){indexi=i;
+    //         break;}
+    //     }
+    //     for(int j=nums.size()-1;j>=0;j--){
+    //         if(nums[j]==target){
+    //             indexj=j;
+    //             break;
+    //         }
+    //     }
+    //     return {indexi,indexj};
+    int first=-1,last=-1;
+    int st=0,end=nums.size()-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(nums[mid]>=target){
+            if(nums[mid]==target) {first=mid;}
+            end=mid-1;
         }
-        for(int j=nums.size()-1;j>=0;j--){
-            if(nums[j]==target){
-                indexj=j;
-                break;
-            }
+        else{st=mid+1;}
+    }
+    st=0,end=nums.size()-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(nums[mid]<=target){
+            if(nums[mid]==target){last=mid;}
+            st=mid+1;
         }
-
-
-        return {indexi,indexj};
+        else{
+            end=mid-1;
+        }
+    }
+    return {first,last};
     }
 };
