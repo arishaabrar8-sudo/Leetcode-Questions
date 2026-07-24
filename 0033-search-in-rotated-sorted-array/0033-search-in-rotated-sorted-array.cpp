d95f123628717afc7f971af1225e37ab66521d3b
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-24 13:44:35 +0500

    Time: 0 ms (100%), Space: 15.3 MB (36.37%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //doing it with bruteforce linear search 
+        int index=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){index=i;}
+        }
+        return index;
+    }
+};
