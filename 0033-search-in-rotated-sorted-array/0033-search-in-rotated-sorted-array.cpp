84e931cf40cadbfcfa6a5c7ed0e367069c8dda41
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-24 14:50:09 +0500

    Time: 0 ms (100%), Space: 15.2 MB (36.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //doing it with bruteforce linear search 
-        int index=-1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){index=i;}
+        // //doing it with bruteforce linear search 
+        // int index=-1;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target){index=i;}
+        // }
+        // return index;
+        
+        //applying binary search
+        int st=0,end=nums.size()-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target) return mid;
+           if(nums[st]<=nums[mid]){
+            if(nums[st]<=target && target<=nums[mid]){
+                end=mid-1;
+            }
+            else{st=mid+1;}
+           }
+           else{
+            if(nums[mid]<=target && target<=nums[end]){
+                st=mid+1;
+            }
+            else{end=mid-1;}
+           }
         }
-        return index;
+        return -1;
     }
 };
