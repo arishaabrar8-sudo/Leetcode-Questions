851b33093240d2000d6000715d117a5ac4ce72dd
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-27 15:56:49 +0500

    Time: 0 ms (100%), Space: 8.7 MB (44.08%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,28 @@
-#include <cmath>
-
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        long long N=n;
+        if(x==1 || n==0) return 1;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        if(x<0){
+            if(N%2==0){x=-x;//as neg multiply neg is pos
+            }
+            else{x=x;}
+        }
+     double ans=1;
+        while(N>0){
+            if(N%2==1){
+                ans=ans*x;
+                N=N-1;
+            }
+            else{
+                x*=x;
+                N/=2;
+            }
+        }
+        return ans;
     }
 };
