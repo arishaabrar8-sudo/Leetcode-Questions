9004c0044d13ad73a40e49aecdf53f250d7ca1c2
arishaabrar8-sudo <arishaabrar8@gmail.com>
2026-07-27 13:56:24 +0500

    Time: 0 ms (100%), Space: 8.7 MB (14.85%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long mySqrt(long long x) {
+        // if(x==0) return 0;
+        // long long ans=1;
+        // for(long long i=1;i<=x;i++){
+        //     if(i*i<=x){ans=i;}
+        // }
+        // return ans; // TLE
+
+        //applying binary search
+        if(x==0) return 0;
+        long st=1,end=x;
+        long  ans=1;
+
+        while(st<=end){
+            long long mid=st+(end-st)/2;
+            long long k=mid*mid;
+            if(k>x){
+                end=mid-1;
+            }
+            else{
+              ans=mid;
+
+                st=mid+1;
+            }
+        }
+        return ans;
+    }
+};
